2baf7040c2f326d77aacbde4d30eec6d7e4b8b9a
Rasik <rasikraj01@gmail.com>
2018-09-27 02:46:16 +0530

    Glut test

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  OpenGL
-//
-//  Created by Rasik Raj on 27/09/18.
-//  Copyright © 2018 Rasik Raj. All rights reserved.
-//
-
+#include <GLUT/GLUT.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+void displayTest(){
+    glClearColor(1,1,0,0);
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glBegin(GL_TRIANGLES);
+    
+    glColor3f(0.5,0,0);
+    
+    glVertex2f(300.0,210.0);
+    glVertex2f(340.0,215.0);
+    glVertex2f(320.0,250.0);
+    
+    glEnd();
+    
+    glFlush();
+}
+
+int main(int argc, char *argv[]){
+    
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,500);
+    glutInitWindowPosition(1,1);
+    
+    glutCreateWindow("Test");
+    
+    glutDisplayFunc(displayTest);
+    glutMainLoop();
 }
