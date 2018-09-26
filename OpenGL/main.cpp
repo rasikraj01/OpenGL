9c425de735557b2aa045da1459dfff0d7765ed2f
Rasik <rasikraj01@gmail.com>
2018-09-27 02:35:29 +0530

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Rasik Raj on 27/09/18.
+//  Copyright © 2018 Rasik Raj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
